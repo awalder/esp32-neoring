3d99fe1a5c0f4f3bf70f26b02165a9c6f068a034
Walder Anssi <anssiwa@gmail.com>
2023-10-24 14:26:28 +0300

    leds with sincos

--- main/main.cpp
+++ main/main.cpp
@@ -1,173 +1,90 @@
 #include "driver/rmt_tx.h"
+#include "encoder.h"
 #include "esp_log.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <cmath>
 #include <vector>
 
-// oneBitHighTime = 48;
-// oneBitLowTime = 48;
-// zeroBitHighTime = 24;
-// zeroBitLowTime = 72;
+static const char* const TAG = "neoring";
 
-// static const int treset = 1000;
+// 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
+#define RMT_LED_STRIP_RESOLUTION_HZ 10000000
 
-struct PulseWidths
-{ // Data transfer times
-        static const int t0h = 32;
-        static const int t0l = 68;
-        static const int t1h = 64;
-        static const int t1l = 36;
-    // static const int t0h = 24;
-    // static const int t0l = 72;
-    // static const int t1h = 48;
-    // static const int t1l = 48;
-    static const int treset = 1000;
-};
-
-struct Vertex
+struct Led
 {
     uint8_t r = 0;
     uint8_t g = 0;
     uint8_t b = 0;
 };
 
-auto setup() -> void
+auto setup()
 {
-    auto channel = rmt_channel_t::RMT_CHANNEL_0;
-
-    auto tx_config = rmt_tx_config_t{
-            .idle_level = RMT_IDLE_LEVEL_LOW,
-            .carrier_en = false,
-            .loop_en = false,
-            .idle_output_en = true,
-    };
-
-    auto rmt_rx = rmt_config_t{
-            .rmt_mode = RMT_MODE_TX,
-            .channel = channel,
-            .gpio_num = GPIO_NUM_3,
-            .clk_div = 1,
-            .mem_block_num = 3,
-            .tx_config = tx_config,
-    };
-
-    ESP_ERROR_CHECK(rmt_config(&rmt_rx));
-    ESP_ERROR_CHECK(rmt_driver_install(channel, 0, 0));
 }
 
 extern "C" auto app_main() -> void
 {
-    const int numLeds = 4;
-    const int bitsPerLed = 24;
-    const int numBits = numLeds * bitsPerLed;
+    ESP_LOGI(TAG, "Create RMT TX channel");
+    rmt_channel_handle_t led_chan = nullptr;
+    rmt_tx_channel_config_t tx_chan_config = {
+            .gpio_num = GPIO_NUM_3,
+            .clk_src = RMT_CLK_SRC_DEFAULT,
+            .resolution_hz = RMT_LED_STRIP_RESOLUTION_HZ,
+            .mem_block_symbols = 64, // increase the block size can make the LED
+                                     // less flickering
+            .trans_queue_depth = 4,  // set the number of transactions that can
+                                     // be pending in the background
+    };
 
-    setup();
-    auto channel = rmt_channel_t::RMT_CHANNEL_0;
-    rmt_item32_t items[numBits];
-    // auto items = std::vector<rmt_item32_t>(numBits);
-    auto data = std::vector<Vertex>(numLeds);
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &led_chan));
 
-    data.at(0).g = 1;
-    data.at(0).r = 0;
-    data.at(0).b = 0;
+    ESP_LOGI(TAG, "Install led strip encoder");
+    rmt_encoder_handle_t led_encoder = nullptr;
+    led_strip_encoder_config_t encoder_config = {
+            .resolution = RMT_LED_STRIP_RESOLUTION_HZ,
+    };
+    ESP_ERROR_CHECK(rmt_new_led_strip_encoder(&encoder_config, &led_encoder));
 
-    ESP_LOGI("main", "bitsPerLed: %d", bitsPerLed);
-    ESP_LOGI("main", "numLeds: %d", numLeds);
-    ESP_LOGI("main", "numBits: %d", numBits);
+    ESP_LOGI(TAG, "Enable RMT TX channel");
+    ESP_ERROR_CHECK(rmt_enable(led_chan));
 
-    for(int i = 0; i < numBits; i++)
+    ESP_LOGI(TAG, "Start LED rainbow chase");
+    rmt_transmit_config_t tx_config = {
+            .loop_count = 0, // no transfer loop
+    };
+
+    const int numLeds = 20 + 16;
+    // const int bitsPerLed = 24;
+    // const int numBits = numLeds * bitsPerLed;
+
+    auto data = std::vector<Led>(numLeds);
+    float brightness = 0.2f;
+
+    while(true) // Infinite loop to keep the wave going
     {
-        auto& item = items[i];
-        item.level0 = 1;
-        item.duration0 = PulseWidths::t0h;
-        item.level1 = 0;
-        item.duration1 = PulseWidths::t0l;
-    }
+        double t = esp_timer_get_time() / 1000000.0; // Current time in seconds
 
-    for(int i = 0; i < numLeds; i++)
-    {
-        auto& vertex = data[i];
-        auto offset = i * bitsPerLed;
-        auto& item = items[offset];
-
-        for(int j = 0; j < 8; j++)
+        for(int i = 0; i < numLeds; ++i)
         {
-        // if(widePixelData & mask)
-        // {
-        //     this->rmtItems[i].duration0 = oneBitHighTime;
-        //     this->rmtItems[i].duration1 = oneBitLowTime;
-        // }
-        // else
-        // {
-        //     this->rmtItems[i].duration0 = zeroBitHighTime;
-        //     this->rmtItems[i].duration1 = zeroBitLowTime;
-        // }
-            uint8_t mask = 1 << (7 - j);
-            uint8_t bit = (vertex.g & mask) != 0;
-            rmt_item32_t& item = items[offset + j];
-            item.level0 = !bit;
-            item.duration0 = bit ? PulseWidths::t1h : PulseWidths::t0h;
-            item.level1 = bit;
-            item.duration1 = bit ? PulseWidths::t1l : PulseWidths::t0l;
+            double phase = i * 2.0 * M_PI / numLeds; // Phase shift for each LED
+            data[i].r = uint8_t((sin(t + phase) * 0.5 + 0.5) * 255 * brightness);
+            data[i].g = uint8_t(
+                    (sin(t + phase + 2.0 * M_PI / 3) * 0.5 + 0.5) * 255 * brightness);
+            data[i].b = uint8_t(
+                    (sin(t + phase + 4.0 * M_PI / 3) * 0.5 + 0.5) * 255 * brightness);
         }
 
-        for(int j = 0; j < 8; j++)
-        {
-            // uint8_t mask = 1 << j;
-            uint8_t mask = 1 << (7 - j);
-            uint8_t bit = (vertex.r & mask) != 0;
-            rmt_item32_t& item = items[offset + 8 + j];
-            item.level0 = !bit;
-            item.duration0 = bit ? PulseWidths::t1h : PulseWidths::t0h;
-            item.level1 = bit;
-            item.duration1 = bit ? PulseWidths::t1l : PulseWidths::t0l;
-        }
+        // Flush RGB values to LEDs
+        ESP_ERROR_CHECK(rmt_transmit(
+                led_chan,
+                led_encoder,
+                data.data(),
+                data.size() * sizeof(Led),
+                &tx_config));
 
-        for(int j = 0; j < 8; j++)
-        {
-            // uint8_t mask = 1 << j;
-            uint8_t mask = 1 << (7 - j);
-            uint8_t bit = (vertex.b & mask) != 0;
-            rmt_item32_t& item = items[offset + 16 + j];
-            item.level0 = !bit;
-            item.duration0 = bit ? PulseWidths::t1h : PulseWidths::t0h;
-            item.level1 = bit;
-            item.duration1 = bit ? PulseWidths::t1l : PulseWidths::t0l;
-        }
+        ESP_ERROR_CHECK(rmt_tx_wait_all_done(led_chan, portMAX_DELAY));
+
+        vTaskDelay(pdMS_TO_TICKS(50)); // Delay to control the speed of the wave
     }
-
-    for(int i = 0; i < bitsPerLed; ++i)
-    {
-        rmt_item32_t& item = items[i];
-        printf("%d", item.level1);
-        if((i + 1) % 8 == 0)
-        {
-            printf(" ");
-        }
-    }
-    printf("\n");
-
-    // {
-    //     static const int tres = 50000; // Assuming a 1MHz RMT clock
-    //
-    //     // Create a reset pulse item
-    //     rmt_item32_t resetItem;
-    //     resetItem.level0 = 0;
-    //     resetItem.duration0 = tres;
-    //     resetItem.level1 = 0;
-    //     resetItem.duration1 = 0; // No second level for the reset item
-    //
-    //     // Insert the reset item at the beginning of the items vector
-    //     items.insert(items.begin(), resetItem);
-    // }
-
-    rmt_wait_tx_done(channel, portMAX_DELAY);
-    // rmt_write_items(channel, items.data(), items.size(), true);
-    esp_err_t ret = rmt_write_items(channel, items, numBits, true);
-    if(ret != ESP_OK)
-    {
-        printf("Error: %s\n", esp_err_to_name(ret));
-    }
-    rmt_wait_tx_done(channel, portMAX_DELAY);
-    // vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
